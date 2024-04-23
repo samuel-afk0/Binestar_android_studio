d9fc9d93daf22c84619c18eb36de0aaca855a6b1
Edward-xd <145070731+Edward-xd@users.noreply.github.com>
2024-04-23 15:47:27 -0600

    Se agregaaron nuevos formularios

--- app/src/main/java/com/example/proyecto_final/RegistroEntrenamiento.java
+++ app/src/main/java/com/example/proyecto_final/RegistroEntrenamiento.java
@@ -7,26 +7,24 @@ import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
 
-public class FormularioMetas extends AppCompatActivity {
+public class RegistroEntrenamiento extends AppCompatActivity {
     Spinner spinner;
     Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario_metas);
+        setContentView(R.layout.activity_registro_entrenamiento);
 
         spinner = findViewById(R.id.spinner);
         submitButton = findViewById(R.id.submit_button);
 
         // Define los elementos del ComboBox
-        String[] items = new String[]{"Perder peso,", "Aumentar la masa muscular", "Mejorar su condición física"};
+        String[] items = new String[]{"Vigorosa", "Moderado", "Ligera"};
 
         // Crea un adaptador para el ComboBox
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         // Asigna el adaptador al ComboBox
         spinner.setAdapter(adapter);
-
-
     }
 }
