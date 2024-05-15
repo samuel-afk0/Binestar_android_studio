dcb1b123baf88e889382d662b6a5b35f56025171
Samuel <134749814+samuel-afk0@users.noreply.github.com>
2024-05-15 11:07:41 -0600

    asi pienso que serie el calendario, me falta la barra de navegacion depues lo modifco

--- app/src/main/java/com/example/proyecto_final/HomeActivity.java
+++ app/src/main/java/com/example/proyecto_final/HomeActivity.java
@@ -1,47 +1,55 @@
 package com.example.proyecto_final;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.github.clans.fab.FloatingActionButton;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+
 import java.util.ArrayList;
 import java.util.List;
-import com.airbnb.lottie.LottieAnimationView;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.motion.widget.MotionLayout;
 
-import com.airbnb.lottie.LottieAnimationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import android.graphics.Color;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import androidx.core.content.ContextCompat;
 public class HomeActivity extends AppCompatActivity {
-    PieChart GraficoAnillo,GraficoAnilloKilometros;
-    Button MenuFlota;
-    private com.github.clans.fab.FloatingActionButton button1, button2, button3, BtnAgregarRecordatorio;
-    private com.github.clans.fab.FloatingActionButton menuFlota;
+    PieChart GraficoAnillo, GraficoAnilloKilometros;
 
+    public int metaKilometros = 0;
+    public double progresoKilometros = 0;
 
-    private boolean areButtonsVisible = false;
+    public  int metaPasos=0, progresoPasos=0;
+    public TextView txtvPasos, txtvKilometros;
+    FloatingActionButton BtnAgregarRecordatorio,btnObjetivo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Iniciar hilo para obtener datos de Firebase
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                obtenerDatosFirebase();
+            }
+        }).start();
+
+        // Configurar BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,63 +58,20 @@ public class HomeActivity extends AppCompatActivity {
                         if (item.getItemId() == R.id.progresos) {
                             startActivity(new Intent(HomeActivity.this, ProgresosActivities.class));
                             return true;
+                        } else if (item.getItemId() == R.id.calendario) {
+                            startActivity(new Intent(HomeActivity.this, CalendarioAnualActivities.class));
+                            return true;
                         }
-                        if(item.getItemId()==R.id.calendario){
-                            startActivity(new Intent(HomeActivity.this, CalendarioAnualActivities.class ));
-                        return true;
-                        }
-                        else {
-
-                            return false;
-                        }
+                        return false;
                     }
-
                 });
-        GraficoAnillo = findViewById(R.id.GraficoAnillo);
-        configurarGraficoAnillo();
-        GraficoAnilloKilometros=findViewById(R.id.GraficoAnilloKilometros);
-        configurarGraficoAnilloKilometros();
-
-        ///menu flotannte
-        // Referencias a los botones en el layout
-        menuFlota = findViewById(R.id.MenuFlota);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        BtnAgregarRecordatorio = findViewById(R.id.BtnAgregarRecordatorio);
-
-        // Configuración del OnClickListener para el botón flotante principal
-        menuFlota.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cambiar la visibilidad de los botones adicionales
-                if (areButtonsVisible) {
-                    button1.setVisibility(View.INVISIBLE);
-                    button2.setVisibility(View.INVISIBLE);
-                    button3.setVisibility(View.INVISIBLE);
-                    BtnAgregarRecordatorio = findViewById(R.id.BtnAgregarRecordatorio);
-                } else {
-                    button1.setVisibility(View.VISIBLE);
-                    button2.setVisibility(View.VISIBLE);
-                    button3.setVisibility(View.VISIBLE);
-                    BtnAgregarRecordatorio = findViewById(R.id.BtnAgregarRecordatorio);
-                }
-                // Cambiar el estado de visibilidad de los botones
-                areButtonsVisible = !areButtonsVisible;
-            }
-        });
-        menuFlota=findViewById(R.id.MenuFlota);
-        menuFlota.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, FormularioMetas.class);
 
 
-                startActivity(intent);
+        /////re direvion de los btonoes de la barra flotante del home activities
 
-            }
-        });
-        //ABRIR ACTIVIDAD RECORDATORIO
+        BtnAgregarRecordatorio=findViewById(R.id.BtnAgregarRecordatorio);
+        btnObjetivo=findViewById(R.id.btnObjetivo);
+
         BtnAgregarRecordatorio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -115,71 +80,126 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-    private void configurarGraficoAnillo() {
-        // Crear datos para el gráfico de dona
-        List<PieEntry> datos = new ArrayList<>();
-        datos.add(new PieEntry(75f, "")); // Progreso (porcentaje)
-        datos.add(new PieEntry(25f, "")); // Espacio en blanco para el resto del círculo
+        btnObjetivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Registro_ActivadFisica.class);
 
-        // Configurar colores
+                startActivity(intent);
+            }
+        });
+
+
+        // Configurar gráficos
+        GraficoAnillo = findViewById(R.id.GraficoAnillo);
+        GraficoAnilloKilometros = findViewById(R.id.GraficoAnilloKilometros);
+    }
+
+    private void obtenerDatosFirebase() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("progreso").document("BaBdwWCKVvzfNhCt7MLM");
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String fecha = documentSnapshot.getString("fecha");
+                    int metaKilogramos = documentSnapshot.getLong("metakilogramos").intValue();
+                    double progresoKilogramos = documentSnapshot.getDouble("progresokilogramos");
+
+                    metaKilometros = documentSnapshot.getLong("metakilometros").intValue();
+                    progresoKilometros = documentSnapshot.getDouble("progresokilometros");
+
+                     metaPasos = documentSnapshot.getLong("metapasos").intValue();
+                     progresoPasos = documentSnapshot.getLong("progresopasos").intValue();
+
+                    Log.d("TAG", "Fecha: " + fecha);
+                    Log.d("TAG", "Meta Kilogramos: " + metaKilogramos);
+                    Log.d("TAG", "Progreso Kilogramos: " + progresoKilogramos);
+                    Log.d("TAG", "Meta Kilometros: " + metaKilometros);
+                    Log.d("TAG", "Progreso Kilometros: " + progresoKilometros);
+                    Log.d("TAG", "Meta Pasos: " + metaPasos);
+                    Log.d("TAG", "Progreso Pasos: " + progresoPasos);
+
+                    // Actualizar gráficos
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            configurarGraficoAnillo();
+                            configurarGraficoAnilloKilometros();
+                        }
+                    });
+                } else {
+                    Log.d("TAG", "El documento no existe");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("TAG", "Error al obtener el documento", e);
+            }
+        });
+    }
+    ////////////////grafica para pasos
+
+    private void configurarGraficoAnillo() {
+
+        float pasosprogres = ((float) progresoPasos / metaPasos) * 100f;
+
+        float porcentajeRestantePasos= 100f - pasosprogres;
+
+        List<PieEntry> datos = new ArrayList<>();
+        datos.add(new PieEntry(pasosprogres, ""));
+        datos.add(new PieEntry(porcentajeRestantePasos, ""));
+
         List<Integer> colores = new ArrayList<>();
         colores.add(getResources().getColor(R.color.colorAccent)); // Color del progreso
-        colores.add(getResources().getColor(android.R.color.transparent)); // Color transparente para el espacio en blanco
+        colores.add(getResources().getColor(android.R.color.transparent)); // Color transparente
 
-        // Crear conjunto de datos
         PieDataSet dataSet = new PieDataSet(datos, "");
         dataSet.setColors(colores);
-        dataSet.setDrawValues(false); // No mostrar valores en las partes del círculo
+        dataSet.setDrawValues(false);
 
-        // Configurar leyenda
         Legend leyenda = GraficoAnillo.getLegend();
-        leyenda.setEnabled(false); // Deshabilitar la leyenda
+        leyenda.setEnabled(false);
 
-        // Crear objeto PieData
         PieData data = new PieData(dataSet);
-
-        // Configurar animación
         GraficoAnillo.animateY(2000);
-
-        // Establecer datos en el gráfico
         GraficoAnillo.setData(data);
-
-        // Actualizar el gráfico
         GraficoAnillo.invalidate();
+
+        txtvPasos=findViewById(R.id.txtvPasos);
+        txtvPasos.setText(progresoPasos+" de "+metaPasos);
     }
+
     private void configurarGraficoAnilloKilometros() {
-        // Crear datos para el gráfico de dona
-        List<PieEntry> datosKilometros = new ArrayList<>();
-        datosKilometros.add(new PieEntry(20f, "")); // Progreso (porcentaje)
-        datosKilometros.add(new PieEntry(80f, "")); // Espacio en blanco para el resto del círculo
+        if (metaKilometros != 0) {
+            float porcentajeProgresoKilometros = (float) (progresoKilometros / metaKilometros) * 100f;
+            float porcentajeRestanteKilometros = 100f - porcentajeProgresoKilometros;
 
-        // Configurar colores
-        List<Integer> coloreskilometros = new ArrayList<>();
-        coloreskilometros.add(getResources().getColor(R.color.bondy)); // Color del progreso
-        coloreskilometros.add(getResources().getColor(android.R.color.transparent)); // Color transparente para el espacio en blanco
+            List<PieEntry> datosKilometros = new ArrayList<>();
+            datosKilometros.add(new PieEntry(porcentajeProgresoKilometros, "Progreso"));
+            datosKilometros.add(new PieEntry(porcentajeRestanteKilometros, "Restante"));
 
-        // Crear conjunto de datos
-        PieDataSet dataSet = new PieDataSet(datosKilometros, "");
-        dataSet.setColors(coloreskilometros);
-        dataSet.setDrawValues(false); // No mostrar valores en las partes del círculo
+            List<Integer> coloreskilometros = new ArrayList<>();
+            coloreskilometros.add(getResources().getColor(R.color.bondy));
+            coloreskilometros.add(getResources().getColor(android.R.color.transparent));
 
-        // Configurar leyenda
-        Legend leyenda = GraficoAnillo.getLegend();
-        leyenda.setEnabled(false); // Deshabilitar la leyenda
+            PieDataSet dataSet = new PieDataSet(datosKilometros, "");
+            dataSet.setColors(coloreskilometros);
+            dataSet.setDrawValues(false);
 
-        // Crear objeto PieData
-        PieData data = new PieData(dataSet);
+            Legend leyenda = GraficoAnilloKilometros.getLegend();
+            leyenda.setEnabled(false);
 
-        // Configurar animación
-        GraficoAnilloKilometros.animateY(2000);
-
-        // Establecer datos en el gráfico
-        GraficoAnilloKilometros.setData(data);
-
-        // Actualizar el gráfico
-        GraficoAnilloKilometros.invalidate();
+            PieData data = new PieData(dataSet);
+            GraficoAnilloKilometros.animateY(2000);
+            GraficoAnilloKilometros.setData(data);
+            GraficoAnilloKilometros.invalidate();
+            txtvKilometros=findViewById(R.id.txtvKilometros);
+            txtvKilometros.setText(progresoKilometros+" de "+metaKilometros);
+        } else {
+            Log.d("TAG", "Error: metaKilometros es igual a cero");
+        }
     }
-
 }
-
