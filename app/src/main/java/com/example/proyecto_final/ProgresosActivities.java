285b8db59f294edb430365181a9d986b9679e4fd
Edward-xd <145070731+Edward-xd@users.noreply.github.com>
2024-06-02 17:23:49 -0600

    se hicieron cambios

--- app/src/main/java/com/example/proyecto_final/ProgresosActivities.java
+++ app/src/main/java/com/example/proyecto_final/ProgresosActivities.java
@@ -1,54 +1,49 @@
 package com.example.proyecto_final;
-import java.util.Arrays;
-import java.util.List;
 
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.formatter.ValueFormatter; // Importación necesaria
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-
-import com.github.mikephil.charting.components.XAxis;
-import android.content.Intent;
-import android.util.Log;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.google.firebase.firestore.CollectionReference;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.graphics.Color;
-
 
 public class ProgresosActivities extends AppCompatActivity {
     private BarChart barChart;
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progresos_activities);
-        LineChart lineChart = findViewById(R.id.lineChart);
+
+        lineChart = findViewById(R.id.lineChart);
+        barChart = findViewById(R.id.barChart);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.progresos);
@@ -57,29 +52,24 @@ public class ProgresosActivities extends AppCompatActivity {
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        if (item.getItemId() == R.id.progresos) {
+                        if (item.getItemId() == R.id.home) {
+                            startActivity(new Intent(ProgresosActivities.this, HomeActivity.class));
                             return true;
-                        } else {
-                            if (item.getItemId() == R.id.home) {
-                                startActivity(new Intent(ProgresosActivities.this, HomeActivity.class));
-                                return true;
-                            } else if (item.getItemId() == R.id.calendario) {
-                                startActivity(new Intent(ProgresosActivities.this, CalendarioAnualActivities.class));
-                                return true;
-                            } else {
-                                return false;
-                            }
+                        } else if (item.getItemId() == R.id.calendario) {
+                            startActivity(new Intent(ProgresosActivities.this, CalendarioAnualActivities.class));
+                            return true;
                         }
+                        return false;
                     }
                 });
 
-
         obtenerDatosFirebase();
-
-        barChart = findViewById(R.id.barChart);
+        configurarGraficoLineas();
         configurarGraficoBarras();
-        mostrarGraficoBarras();
-        // Datos de ejemplo: calorías al mes
+    }
+
+    private void configurarGraficoLineas() {
+        // Configurar el gráfico de líneas
         List<Entry> entries = new ArrayList<>();
         entries.add(new Entry(1, 1500));
         entries.add(new Entry(2, 800));
@@ -93,84 +83,61 @@ public class ProgresosActivities extends AppCompatActivity {
         entries.add(new Entry(11, 2700));
         entries.add(new Entry(12, 50));
 
-        // Configurar el conjunto de datos
         LineDataSet dataSet = new LineDataSet(entries, "Calorías al mes");
         dataSet.setColor(Color.BLUE);
-        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER); // Configurar el modo como CUBIC_BEZIER para una apariencia de montaña
-        dataSet.setFillColor(Color.BLUE); // Color del relleno bajo la curva
-        dataSet.setFillAlpha(50); // Transparencia del relleno (0-255)
-        dataSet.setDrawFilled(true); // Habilitar el dibujo del área rellena
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setFillColor(Color.BLUE);
+        dataSet.setFillAlpha(50);
+        dataSet.setDrawFilled(true);
 
-// Configurar la línea de datos
         LineData lineData = new LineData(dataSet);
         lineChart.setData(lineData);
 
-        // Configurar ejes
         XAxis xAxis = lineChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
         YAxis yAxisRight = lineChart.getAxisRight();
-        yAxisRight.setEnabled(false); // Deshabilitar eje Y derecho
+        yAxisRight.setEnabled(false);
 
-        // Configurar descripción
         Description description = new Description();
         description.setText("Meses");
         lineChart.setDescription(description);
 
-        // Configurar leyenda
         Legend legend = lineChart.getLegend();
         legend.setEnabled(true);
 
-// Animaciones
         lineChart.animateX(1000);
-        configurarGraficoLineas2();
     }
 
     private void configurarGraficoBarras() {
-        XAxis xAxis = barChart.getXAxis(); // Obtener el eje X desde el BarChart
+        // Configurar el gráfico de barras
+        XAxis xAxis = barChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setGranularity(1f);
         xAxis.setDrawGridLines(false);
-        xAxis.setValueFormatter(new ValueFormatter() {
-            final String[] diasSemana = {"L", "M", "X", "J", "V", "S", "D"};
-
-            @Override
-            public String getFormattedValue(float value) {
-                int index = (int) value;
-                if (index >= 0 && index < diasSemana.length) {
-                    return diasSemana[index];
-                }
-                return "";
-            }
-        });
-
-        xAxis.setTextColor(Color.WHITE); // Color del texto del eje X
+        xAxis.setTextColor(Color.WHITE);
 
         YAxis leftAxis = barChart.getAxisLeft();
         YAxis rightAxis = barChart.getAxisRight();
         leftAxis.setDrawGridLines(true);
         rightAxis.setDrawGridLines(false);
-    }
 
-    private void configurarGraficoBarras(List<Float> valores) {
+        List<Float> valores = obtenerValoresDeDatosPredefinidos();
         ArrayList<BarEntry> entradas = new ArrayList<>();
         for (int i = 0; i < valores.size(); i++) {
             entradas.add(new BarEntry(i, valores.get(i)));
         }
+
         BarDataSet dataSet = new BarDataSet(entradas, "Ejercicio por día (minutos)");
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS); // Colores predefinidos
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
         BarData data = new BarData(dataSet);
         barChart.setData(data);
         barChart.invalidate();
     }
 
-    private void mostrarGraficoBarras() {
-        List<Float> valores = obtenerValoresDeDatosPredefinidos();
-        configurarGraficoBarras(valores);
-    }
-
     private List<Float> obtenerValoresDeDatosPredefinidos() {
-        // Definir los valores predefinidos para cada día de la semana
+        // Valores predefinidos para cada día de la semana
         List<Float> valores = new ArrayList<>();
         valores.add(120f); // Lunes
         valores.add(90f);  // Martes
@@ -181,56 +148,7 @@ public class ProgresosActivities extends AppCompatActivity {
         valores.add(110f); // Domingo
         return valores;
     }
-    private void configurarGraficoLineas2() {
-        // Obtener la referencia del gráfico de líneas lineChart2
-        LineChart lineChart2 = findViewById(R.id.lineChart2);
 
-        // Datos de ejemplo para el gráfico de líneas
-        List<Entry> entries2 = new ArrayList<>();
-        entries2.add(new Entry(1, 1500)); // Enero
-        entries2.add(new Entry(2, 800));  // Febrero
-        entries2.add(new Entry(3, 1900)); // Marzo
-        entries2.add(new Entry(4, 50));   // Abril
-        entries2.add(new Entry(5, 300));  // Mayo
-        entries2.add(new Entry(6, 2100)); // Junio
-
-        // Configurar el conjunto de datos para el gráfico de líneas
-        LineDataSet dataSet2 = new LineDataSet(entries2, "Calorías por mes");
-        dataSet2.setColor(Color.RED);
-        LineData lineData2 = new LineData(dataSet2);
-        lineChart2.setData(lineData2);
-
-        // Formatear el eje X para mostrar los meses
-        XAxis xAxis2 = lineChart2.getXAxis();
-        xAxis2.setValueFormatter(new ValueFormatter() {
-            final String[] meses = {"Ene", "Feb", "Mar", "Abr", "May", "Jun"};
-
-            @Override
-            public String getFormattedValue(float value) {
-                int index = (int) value;
-                if (index >= 0 && index < meses.length) {
-                    return meses[index];
-                }
-                return "";
-            }
-        });
-
-        // Deshabilitar el eje Y derecho
-        YAxis yAxisRight2 = lineChart2.getAxisRight();
-        yAxisRight2.setEnabled(false);
-
-        // Configurar la descripción
-        Description description2 = new Description();
-        description2.setText("Meses");
-        lineChart2.setDescription(description2);
-
-        // Configurar la leyenda
-        Legend legend2 = lineChart2.getLegend();
-        legend2.setEnabled(true);
-
-        // Animar el gráfico
-        lineChart2.animateX(1000);
-    }
     private void obtenerDatosFirebase() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference progresoRef = db.collection("progreso").document("BaBdwWCKVvzfNhCt7MLM");
@@ -244,8 +162,12 @@ public class ProgresosActivities extends AppCompatActivity {
 
                 for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                     Double calorias = document.getDouble("calorias");
-                    Integer pasos = document.getLong("pasos").intValue();
+                    Long pasosLong = document.getLong("pasos");
                     Double kilometros = document.getDouble("kilometros");
+
+                    // Verificar si el valor de pasosLong es nulo antes de llamar a intValue()
+                    int pasos = pasosLong != null ? pasosLong.intValue() : 0;
+
                     pasosList.add(pasos);
                     caloriasList.add(calorias);
                     kilometrosList.add(kilometros);
@@ -261,9 +183,4 @@ public class ProgresosActivities extends AppCompatActivity {
             }
         });
     }
-
-
-
-
-
 }
