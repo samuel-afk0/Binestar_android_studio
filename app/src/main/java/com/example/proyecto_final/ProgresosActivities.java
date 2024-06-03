795f8fdda52930ac1c63b901a10aa020c19cd7fb
Edward-xd <145070731+Edward-xd@users.noreply.github.com>
2024-06-02 20:45:09 -0600

    se hicieron cambios en progresos activitis

--- app/src/main/java/com/example/proyecto_final/ProgresosActivities.java
+++ app/src/main/java/com/example/proyecto_final/ProgresosActivities.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
@@ -21,6 +22,9 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -29,21 +33,30 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.github.mikephil.charting.charts.PieChart;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProgresosActivities extends AppCompatActivity {
-    private BarChart barChart;
-    private LineChart lineChart;
+    private BarChart barChart, barChart2, barChart3;
+    private LineChart lineChart, lineChart2, lineChart3;
 
+    private PieChart pieChart,pieChart2,pieChart3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progresos_activities);
 
         lineChart = findViewById(R.id.lineChart);
+        lineChart2 = findViewById(R.id.lineChart2);
+        lineChart3 = findViewById(R.id.lineChart3);
         barChart = findViewById(R.id.barChart);
+        barChart2 = findViewById(R.id.barChart2);
+        barChart3 = findViewById(R.id.barChart3);
+        pieChart = findViewById(R.id.pieChart);
+        pieChart2 = findViewById(R.id.pieChart2);
+        pieChart3 = findViewById(R.id.pieChart3);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.progresos);
@@ -64,24 +77,15 @@ public class ProgresosActivities extends AppCompatActivity {
                 });
 
         obtenerDatosFirebase();
-        configurarGraficoLineas();
-        configurarGraficoBarras();
     }
 
-    private void configurarGraficoLineas() {
+    private void configurarGraficoLineas(List<Double> caloriasList, LineChart lineChart) {
         // Configurar el gráfico de líneas
         List<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(1, 1500));
-        entries.add(new Entry(2, 800));
-        entries.add(new Entry(3, 1900));
-        entries.add(new Entry(5, 50));
-        entries.add(new Entry(6, 300));
-        entries.add(new Entry(7, 2100));
-        entries.add(new Entry(8, 400));
-        entries.add(new Entry(9, 2000));
-        entries.add(new Entry(10, 2300));
-        entries.add(new Entry(11, 2700));
-        entries.add(new Entry(12, 50));
+        int cantidadDeDatos = lineChart == lineChart3 ? caloriasList.size() : lineChart == lineChart2 ? 15 : 8;
+        for (int i = 0; i < cantidadDeDatos; i++) {
+            entries.add(new Entry(i, caloriasList.get(i).floatValue()));
+        }
 
         LineDataSet dataSet = new LineDataSet(entries, "Calorías al mes");
         dataSet.setColor(Color.BLUE);
@@ -107,9 +111,10 @@ public class ProgresosActivities extends AppCompatActivity {
         legend.setEnabled(true);
 
         lineChart.animateX(1000);
+        lineChart.invalidate();
     }
 
-    private void configurarGraficoBarras() {
+    private void configurarGraficoBarras(List<Integer> pasosList, BarChart barChart) {
         // Configurar el gráfico de barras
         XAxis xAxis = barChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -122,10 +127,10 @@ public class ProgresosActivities extends AppCompatActivity {
         leftAxis.setDrawGridLines(true);
         rightAxis.setDrawGridLines(false);
 
-        List<Float> valores = obtenerValoresDeDatosPredefinidos();
         ArrayList<BarEntry> entradas = new ArrayList<>();
-        for (int i = 0; i < valores.size(); i++) {
-            entradas.add(new BarEntry(i, valores.get(i)));
+        int cantidadDeDatos = barChart == barChart3 ? pasosList.size() : barChart == barChart2 ? 15 : 8;
+        for (int i = 0; i < cantidadDeDatos; i++) {
+            entradas.add(new BarEntry(i, pasosList.get(i)));
         }
 
         BarDataSet dataSet = new BarDataSet(entradas, "Ejercicio por día (minutos)");
@@ -136,17 +141,39 @@ public class ProgresosActivities extends AppCompatActivity {
         barChart.invalidate();
     }
 
-    private List<Float> obtenerValoresDeDatosPredefinidos() {
-        // Valores predefinidos para cada día de la semana
-        List<Float> valores = new ArrayList<>();
-        valores.add(120f); // Lunes
-        valores.add(90f);  // Martes
-        valores.add(150f); // Miércoles
-        valores.add(80f);  // Jueves
-        valores.add(100f); // Viernes
-        valores.add(130f); // Sábado
-        valores.add(110f); // Domingo
-        return valores;
+    private void configurarGraficoPastel(List<Double> kilometrosList, PieChart pieChart) {
+        // Calcular el total de kilómetros
+        double totalKilometros = 0;
+        for (Double kilometros : kilometrosList) {
+            totalKilometros += kilometros;
+        }
+
+        // Configurar los datos para el gráfico de pastel
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        int cantidadDeDatos = 0;
+        if (pieChart == pieChart2) {
+            cantidadDeDatos = 15;
+        } else if (pieChart == pieChart3) {
+            cantidadDeDatos = kilometrosList.size();
+        } else {
+            cantidadDeDatos = 7;
+        }
+
+        for (int i = 0; i < cantidadDeDatos; i++) {
+            double porcentaje = (kilometrosList.get(i) / totalKilometros) * 100;
+            entries.add(new PieEntry((float) porcentaje, "Día " + (i + 1)));
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Kilómetros por día");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        // Configurar el gráfico
+        pieChart.setData(data);
+        pieChart.setDrawEntryLabels(false); // Deshabilitar etiquetas en las entradas
+        pieChart.getDescription().setEnabled(false); // Deshabilitar descripción
+        pieChart.animateY(1000);
+        pieChart.invalidate();
     }
 
     private void obtenerDatosFirebase() {
@@ -158,23 +185,35 @@ public class ProgresosActivities extends AppCompatActivity {
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 List<Integer> pasosList = new ArrayList<>();
                 List<Double> caloriasList = new ArrayList<>();
-                List<Double> kilometrosList = new ArrayList<>();
+                List<Double> kilometrosList = new ArrayList<>(); // Agregar lista para los kilómetros
 
                 for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                     Double calorias = document.getDouble("calorias");
                     Long pasosLong = document.getLong("pasos");
-                    Double kilometros = document.getDouble("kilometros");
+                    Double kilometros = document.getDouble("kilometros"); // Recoger los kilómetros
 
-                    // Verificar si el valor de pasosLong es nulo antes de llamar a intValue()
                     int pasos = pasosLong != null ? pasosLong.intValue() : 0;
 
                     pasosList.add(pasos);
                     caloriasList.add(calorias);
                     kilometrosList.add(kilometros);
                 }
+
                 Log.d("TAG", "Pasos: " + pasosList.toString());
                 Log.d("TAG", "Calorias: " + caloriasList.toString());
                 Log.d("TAG", "Kilometros: " + kilometrosList.toString());
+
+                configurarGraficoLineas(caloriasList, lineChart);
+                configurarGraficoLineas(caloriasList, lineChart2);
+                configurarGraficoLineas(caloriasList, lineChart3);
+                configurarGraficoBarras(pasosList, barChart);
+                configurarGraficoBarras(pasosList, barChart2);
+                configurarGraficoBarras(pasosList, barChart3);
+                configurarGraficoPastel(kilometrosList, pieChart);
+                configurarGraficoPastel(kilometrosList, pieChart2);
+                configurarGraficoPastel(kilometrosList, pieChart3);
+
+                // Puedes agregar configuración de gráfico para los kilómetros aquí si es necesario
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -183,4 +222,5 @@ public class ProgresosActivities extends AppCompatActivity {
             }
         });
     }
+
 }
